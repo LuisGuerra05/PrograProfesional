5f0e9a3865fd36cc037429363f10f660b8ce83e2
LuisGuerra05 <luguerra@alumnos.uai.cl>
2025-04-24 19:16:08 -0400

    Más sinonimos

--- client/src/pages/ProductsPage/ProductsPage.js
+++ client/src/pages/ProductsPage/ProductsPage.js
@@ -6,7 +6,6 @@ import AlgoliaWrapper from '../../components/search/AlgoliaWrapper';
 const ProductsPage = () => {
   return (
     <div className="main-content" style={{ padding: '30px' }}>
-      <h2 style={{ textAlign: 'center', marginBottom: '20px' }}>Camisetas Disponibles</h2>
       <AlgoliaWrapper />
     </div>
   );
