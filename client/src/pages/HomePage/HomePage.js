850a0992270f2968511844cef907bfb736c681ff
LuisGuerra05 <luguerra@alumnos.uai.cl>
2025-05-17 13:50:59 -0400

    EK-109: Ajuste responsive en Home Page para mostrar 2 escudos por fila en móviles
    Se modificó la distribución de los escudos en la página principal para mejorar la experiencia en dispositivos móviles.
    Ahora, en pantallas pequeñas (xs), se muestran dos escudos por fila en lugar de uno, utilizando el sistema de columnas de Bootstrap (xs={6}).

--- client/src/pages/HomePage/HomePage.js
+++ client/src/pages/HomePage/HomePage.js
@@ -1,8 +1,11 @@
+// frontend/pages/HomePage.js
+
 import React from 'react';
 import './HomePage.css';
 import PromotionsCarousel from '../../components/carousel/PromotionsCarousel';
 import { useTranslation } from 'react-i18next';
 import { teams } from '../../utils/imageHelpers';
+import { Row, Col } from 'react-bootstrap';
 
 function HomePage() {
   const { t } = useTranslation();
@@ -12,19 +15,23 @@ function HomePage() {
       <PromotionsCarousel />
       <h2 className="homepage-title">{t('title')}</h2>
       <p className="homepage-subtitle">{t('subtitle')}</p>
+
       <section className="home-content">
-        <div className="grid-container">
+        <Row className="justify-content-center gx-4 gy-4 px-4">
           {teams.map((team, index) => (
-            <div
+            <Col
               key={index}
-              className="grid-item"
-              onClick={() => window.location.href = `/products?team=${encodeURIComponent(team.name)}`}
+              xs={5} sm={4} md={3} lg={2}
+              className="d-flex flex-column align-items-center"
+              onClick={() =>
+                window.location.href = `/products?team=${encodeURIComponent(team.name)}`
+              }
             >
               <img src={team.logo} alt={`${team.name} Escudo`} className="team-logo" />
-              <p>{team.name}</p>
-            </div>
+              <p className="text-center">{team.name}</p>
+            </Col>
           ))}
-        </div>
+        </Row>
       </section>
     </div>
   );
